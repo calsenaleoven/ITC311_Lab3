4a697de3a1cf5ec6fe1eca907cfe9057fe53dde8
vhenz1017 <calsenaleoven@gmail.com>
2023-09-29 15:46:36 +0800

    initial commit
    develop

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,7 @@
+<?= $this->include('e-commerce/include/top') ?>
+<div class="">
+  <?= $this->include('e-commerce/include/header') ?>
+  <?= $this->include('e-commerce/include/hero') ?>
+
+</div>
+<?= $this->include('e-commerce/include/end') ?>
